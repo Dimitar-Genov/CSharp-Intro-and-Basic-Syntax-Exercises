0c78732af80eb549426a4ef49a2b6e5300101785
Dimitar Genov <dimitarkostadinovgenov@gmail.com>
2017-10-02 03:51:42 +0300

    CSharp-Intro-and-Basic-Syntax-Exercises

---
+++ Problem 1. Debit Card Number/Problem 1. Debit Card Number.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1.Debit_Card_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int seccondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            int fourthNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{firstNumber:D4} {seccondNumber:D4} {thirdNumber:D4} {fourthNumber:D4}");
+
+        }
+    }
+}
---
+++ Problem 4. Beverage Labels/Problem 4. Beverage Labels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_4.Beverage_Labels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int volume = int.Parse(Console.ReadLine());
+            int energy = int.Parse(Console.ReadLine());
+            int sugar = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{volume}ml {name}:{Environment.NewLine}{energy * volume / 100.0}kcal, {sugar * volume / 100.0}g sugars");
+        }
+    }
+}
